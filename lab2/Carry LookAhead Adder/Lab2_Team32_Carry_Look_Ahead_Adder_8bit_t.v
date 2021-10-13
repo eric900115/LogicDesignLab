6bae68cd1947fda1dd4111be1d6bcb0a14f2a3da
eric <eric20607@gapp.nthu.edu.tw>
2021-10-14 05:52:07 +0800

    update_CLA_tb

--- lab2/Carry LookAhead Adder/Lab2_Team32_Carry_Look_Ahead_Adder_8bit_t.v
+++ lab2/Carry LookAhead Adder/Lab2_Team32_Carry_Look_Ahead_Adder_8bit_t.v
@@ -5,17 +5,7 @@ reg [7:0] a = 8'b0;
 reg [7:0] b = 8'b0;
 reg cin = 1'b0;
 wire [7:0] sum;
-wire [1:0] p, g;
 wire cout;
-wire pg, gg;
-/*
-Ripple_Carry_Adder rca(
-    .a (a),
-    .b (b),
-    .cin (cin),
-    .cout (cout),
-    .sum (sum)
-);*/
 
  Carry_Look_Ahead_Adder_8bit cla(
     .a(a), 
@@ -24,13 +14,7 @@ Ripple_Carry_Adder rca(
     .sum(sum), 
     .c8(cout)
 );
-//CLA_4 m0(a, b, cin, cout, sum, pg, gg);
-//CLA_4 cla0(a[3:0], b[3:0], cin, cout, sum[3:0], p[0], g[0]);
-// uncommment and add "+access+r" to your nverilog command to dump fsdb waveform on NTHUCAD
-// initial begin
-//      $fsdbDumpfile("Adders.fsdb");
-//      $fsdbDumpvars;
-// end
+
 
 initial begin
   repeat (2 ** 16) begin
@@ -46,8 +30,6 @@ initial begin
 	if(a + b + cin != {cout, sum})
 		$display("error occurs at a=%b b=%b cin=%b cout=%b sum=%b", a, b, cin, cout,sum);
   end
-  #1 {a, b} = 16'b0;
-   #1 $finish;
-  
+  #1 $finish;
 end  
 endmodule
