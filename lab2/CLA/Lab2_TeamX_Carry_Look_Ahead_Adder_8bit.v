009f868cd1b6137eaf31e0ee4b3da17498f3093a
blachang <snake96274@gapp.nthu.edu.tw>
2021-10-14 03:38:53 +0800

    lab2

--- lab2/CLA/Lab2_TeamX_Carry_Look_Ahead_Adder_8bit.v
+++ lab2/CLA/Lab2_TeamX_Carry_Look_Ahead_Adder_8bit.v
@@ -6,59 +6,66 @@ input c0;
 output [7:0] s;
 output c8;
 
-wire c;
+wire c4;
+wire [1:0] pg, gg;
 
-CLA cla0(a[3:0], b[3:0], c0, c, s[3:0]);
-CLA cla1(a[4:7], b[4:7], c, c8, s[4:7]);
+CLA_4 cla0(a[3:0], b[3:0], c0, c4, s[3:0], pg[0], gg[0]);
+CLA_4 cla1(a[7:4], b[7:4], c4, c8, s[7:4], pg[1], gg[1]);
 
-
+CLA_2bits cla_2bits({c8, c4}, pg, gg, c0);
 
 endmodule
 
-module CLA(a, b, cin, cout, sum);
+module CLA_4(a, b, cin, cout, sum, pg, gg);
 
 parameter SIZE = 4;
 input [SIZE-1:0] a, b;
 input cin;
 output [SIZE-1:0]sum;
-output cout;
+output cout, pg, gg;
 
 wire [SIZE-1:0] p, g;
 wire [SIZE-1-1:0] c;
 
-Full_Adder fa0(sum[0], p[0], g[0], a[0], b[0], cin);
-Full_Adder fa1(sum[1], p[1], g[1], a[1], b[1], c[0]);
-Full_Adder fa2(sum[2], p[2], g[2], a[2], b[2], c[1]);
-Full_Adder fa3(sum[3], p[3], g[3], a[3], b[3], c[2]);
+Full_Adder fa0(sum[0], a[0], b[0], cin);
+Full_Adder fa1(sum[1], a[1], b[1], c[0]);
+Full_Adder fa2(sum[2], a[2], b[2], c[1]);
+Full_Adder fa3(sum[3], a[3], b[3], c[2]);
 
-CLA_4bits cla2({cout, c[2:0]}, p, g, cin);
+OR aORb[3:0](a, b, p);
+AND aANDb[3:0](a, b, g);
+
+CLA_4bits cla2({cout, c[2:0]}, p, g, cin, pg, gg);
 
 endmodule
 
-module CLA_2bits(cout, p, g, cin);
+module CLA_2bits(cout, pg, gg, cin);
 parameter SIZE = 2;
 
-input [SIZE-1:0] p, g;
+input [SIZE-1:0] pg, gg;
 input cin;
+//cout[0] == c4, cout[1] == c8
 output [SIZE-1:0] cout;
 
-wire [5:0] w;
-wire [SIZE-1:0] gbar;
-
-NOT ng[SIZE-1:0](g, gbar);
-
-// c4 = g3 + (p3 * c3)
+// c4 = c0 * (pg[0,3] + gg[0,3])
+wire pg_or_gg0, pg_or_gg1;
+OR or_pg_gg0(pg[0], gg[0], pg_or_gg0);
+AND and_c0_pggg(pg_or_gg0, cin, cout[0]);
+// c8 = c4 * (pg[4,7] + gg[4,7])
+OR or_pg_gg4(pg[1], gg[1], pg_or_gg1);
+AND and_pg_gg4(pg_or_gg1, cout[0], cout[1]);
 
 endmodule
 
-module CLA_4bits(cout, p, g, cin);
+module CLA_4bits(cout, p, g, cin, pg, gg);
 parameter SIZE = 4;
 
 input [SIZE-1:0] p, g;
 input cin;
 output [SIZE-1:0] cout;
+output pg, gg;
 
-wire [9:0] w;
+wire [20:0] w;
 wire [SIZE-1:0] gbar;
 
 NOT ng[SIZE-1:0](g, gbar);
@@ -70,45 +77,81 @@ OR gorw0(g[0], w[0], cout[0]);
 // c2 = g1 + (p1 * c1)
 //    = g1 + (g0 * p1) + (c0 * p0 * p1)
 //    = !(!g1 * !(g0 * p1) * !(c0 * p0 * p1))
-nand c2_0 (w[1], g[0], p[1]);
+assign cout[2] = g[1] | (g[0] & p[1]) | (cin & p[0] & p[1]);
+
+/*AND and_g0_p1(g[0], p[1], w[1]);
+AND and1(cin, p[0], w[2]);
+AND and2(w[2], p[1], w[3]);
+OR or0(g[1], w[1], w[4]);
+OR or1(w[4], w[3], cout[1]);*/
+/*nand c2_0 (w[1], g[0], p[1]);
 nand c2_1 (w[2], cin, p[0], p[1]);
 nand c2_3 (cout[1], gbar[1], w[1], w[2]);
-
+*/
 // c3 = g2 + (p2 * c2)
 //    = g2 + (g1 * p2) + (g0 * p1 * p2) + (c0 * p0 * p1 * p2)
 //    = !(!g2 * !(g1 * p2) * !(g0 * p1 * p2) * !(c0 * p0 * p1 * p2))
-nand c3_0 (w[3], g[1], p[2]);
-nand c3_1 (w[4], g[0], p[2], p[1]);
-nand c3_2 (w[5], cin, p[2], p[1], p[0]);
-nand c3_3 (cout[1], gbar[2], w[3], w[4], w[5]);
-
+assign cout[3] = g[2] | (g[1] & p[2]) | (g[0] & p[1] & p[2]) | (cin & p[0] & p[1] & p[2]);
+/*AND and3(g[1], p[2], w[5]);//
+AND and4(g[0], p[1], w[6]);
+AND and5(w[6], p[2], w[7]);//
+AND and6(cin, p[0], w[8]);
+AND and7(w[8], p[1], w[9]);
+AND and8(w[9], p[2], w[10]);//
+OR or2(g[2], w[5], w[11]);
+OR or3(w[11], w[7], w[12]);
+OR or4(w[12], w[10], cout[2]);*/
+/*nand c3_0 (w[3], g[1], p[2]);
+nand c3_1 (w[4], g[0], p[1], p[2]);
+nand c3_2 (w[5], cin, p[0], p[1], p[2]);
+nand c3_3 (cout[2], gbar[2], w[3], w[4], w[5]);
+*/
 // c4 = g3 + (p3 * c3)
 //    = g3 + (g2 * p3) + (g1 * p3 * p2) + (g0 * p3 * p2 * p1) + (cin * p3 * p2 * p1 * p0)
 //    = !(!g3 * !(g2 * p3) * !(g1 * p3 * p2) * !(g0 * p3 * p2 * p1) * !(cin * p3 * p2 * p1 * p0))
-nand c4_0 (w[6], g[2], p[3]);
+assign cout[4] = g[3] | (g[2] & p[3]) | (g[1] & p[3] & p[2]) | (g[0] & p[3] & p[2] & p[1]) + (cin & p[3] & p[2] & p[1] & p[0]);
+/*and and9(w[14], g[2], p[3]);
+and and10(w[15], g[1], p[3], p[2]);
+and and11(w[16], cin, p[3], p[2], p[1], p[0]);
+or or5(cout[3], w[14], w[15], w[16]);*/
+/*nand c4_0 (w[6], g[2], p[3]);
 nand c4_1 (w[7], g[1], p[3], p[2]);
 nand c4_2 (w[8], g[0], p[3], p[2], p[1]);
 nand c4_3 (w[9], cin, p[3], p[2], p[1], p[0]);
 nand c4_4 (cout[3], gbar[3], w[6], w[7], w[8], w[9]);
+*/
+// pg = p0 * p1 * p2 * p3
+wire p0_and_p1, p2_and_p3;
+AND and_p0_p1(p[0], p[1], p0_and_p1);
+AND and_p2_p3(p[2], p[3], p2_and_p3);
+AND and_p0p1_p2p3(p0_and_p1, p2_and_p3, pg);
+
+wire [3:0] wgp;
+// gg = g3 + g2 * p3 + g1 * p3 * p2 + g0 * p3 * p2 * p1
+//    = !(!g3 * !(g2 * p3) * !(g1 *p3 * p2) * !(g0 * p3 *p2 * p1))
+nand nand_gg_0(wgp[2], g[2], p[3]);
+nand nand_gg_1(wgp[1], g[1], p[3], p[2]);
+nand nand_gg_2(wgp[0], g[0], p[3], p[2], p[1]);
+nand nand_gg(gg, gbar[3], wgp[2], wgp[1], wgp[0]);
 
 endmodule
 
-module Full_Adder (sum, p, g, a, b, cin);
+module Full_Adder (sum, a, b, cin);
 input a, b, cin;
-output p, g, sum;
-
-wire an, bn, cn;
-wire abc, a_bn_cn, an_b_cn, an_bn_c;
+output sum;
  
-OR aORb(a, b, p);
-AND aANDb(a, b, g);
+wire w0, abnand, acnand, bcnand;
+ 
+//OR aORb(a, b, p);
+//AND aANDb(a, b, g);
 
-NOT na(a, an), nb(b, bn), nc(c, cn);
+XOR xor0(a, b, w0);
+XOR xor1(w0, cin, sum);
 
-nand nabc(abc, a, b, cin);
-nand abncn(a_bn_cn, a, bn, cn), anbcn(an_b_cn, an, b, cn), anbnc(an_bn_c, an, bn, cin);
-nand sumcal(sum, abc, a_bn_cn, an_b_cn, an_bn_c);
-
+/*nand nandab(abnand, a, b);
+nand nandac(acnand, a, cin);
+nand nandbc(bcnand, b, cin);
+nand nandall(cout, abnand, acnand, bcnand);*/
 
 endmodule
 
@@ -138,14 +181,5 @@ output f;
 wire w0, w1, w2, w3;
 nand nand0(w0, a, a), nand1(w1, b, b);
 nand nand2(w2, w0, b), nand3(w3, a, w1);
-nand nand4(f, w2, w3);
-endmodule
-
-module XNOR(a, b, f);
-input a, b;
-output f;
-wire w0, w1, w2, w3;
-nand nand0(w0, a, a), nand1(w1, b, b);
-nand nand2(w2, w0, w1), nand3(w3, a, b);
 nand nand4(f, w2, w3);
 endmodule
---
+++ lab2/CLA/cla.v
@@ -0,0 +1,189 @@
+`timescale 1ns/1ps
+
+module Carry_Look_Ahead_Adder_8bit(a, b, c0, sum, c8);
+input [7:0] a, b;
+input c0;
+output [7:0] sum;
+output c8;
+
+wire c4;
+wire [1:0] pg, gg;
+
+CLA_4 cla0(a[3:0], b[3:0], c0, c4, sum[3:0], pg[0], gg[0]);
+CLA_4 cla1(a[7:4], b[7:4], c4, c8, sum[7:4], pg[1], gg[1]);
+
+CLA_2bits cla_2bits({c8, c4}, pg, gg, c0);
+
+endmodule
+
+module CLA_4(a, b, cin, cout, sum, pg, gg);
+
+input [3:0] a, b;
+input cin;
+output [3:0]sum;
+output cout, pg, gg;
+
+wire [3:0] p, g;
+wire [2:0] c;
+
+Full_Adder fa0(sum[0], a[0], b[0], cin);
+Full_Adder fa1(sum[1], a[1], b[1], c[0]);
+Full_Adder fa2(sum[2], a[2], b[2], c[1]);
+Full_Adder fa3(sum[3], a[3], b[3], c[2]);
+
+OR aORb[3:0](a, b, p);
+AND aANDb[3:0](a, b, g);
+
+CLA_4bits cla2({cout, c[2:0]}, p, g, cin, pg, gg);
+
+endmodule
+
+module CLA_4bits(cout, p, g, cin, pg, gg);
+input [3:0] p, g;
+input cin;
+output [3:0] cout;
+output pg, gg;
+
+wire [18:0]w;
+
+//c1
+AND and_c1(p[0], cin, w[0]);
+OR or_c1(g[0], w[0], cout[0]);
+
+//c2
+AND and_c2(p[1], g[0], w[2]);
+AND_4bits and_c21(p[1], p[0], cin, 1'b1, w[3]);
+OR_4bits or_c2(g[1], w[2], w[3], 1'b0, cout[1]);
+
+//c3
+AND and_c3(p[2], g[1], w[4]);
+AND_4bits and_c4(p[2], p[1], g[0], 1'b1, w[5]);
+AND_4bits and_c5(p[2], p[1], p[0], cin, w[6]);
+OR_4bits or_c3(g[2], w[4], w[5], w[6], cout[2]);
+
+//c4
+AND and_c40(p[3], g[2], w[7]);
+AND_4bits and_c41(p[3], p[2], g[1], 1'b1, w[8]);
+AND_4bits and_c42(p[3], p[2], p[1], g[0], w[9]);
+AND_4bits and_c43(p[3], p[2], p[1], p[0], w[10]);
+AND and_c44(w[10], cin, w[11]);
+OR_4bits or_c4(w[7], w[8], w[9], w[11], w[12]);
+OR or_c41(w[12], g[3], cout[3]);
+
+//pg
+AND_4bits and_pg(p[0], p[1], p[2], p[3], pg);
+
+//gg
+AND_4bits and_gg(g[1], p[3], p[2], 1'b1, w[14]);
+AND_4bits and_gg1(g[0], p[3], p[2], p[1], w[15]);
+AND and_gg2(g[2], p[3], w[16]);
+OR_4bits or_gg(g[3], w[14], w[15], w[16], gg);
+
+endmodule
+
+module Full_Adder(sum, a, b, cin);
+input a, b, cin;
+output sum;
+
+wire a_xor_b, w1, w2, w3, w4;
+
+XOR xor_a_b(a, b, a_xor_b);
+XOR xor_a_b_cin(a_xor_b, cin, sum);
+
+endmodule
+
+module CLA_2bits(cout, pg, gg, cin);
+input [1:0] pg, gg;
+input cin;
+//cout[0] == c4, cout[1] == c8
+output [1:0] cout;
+/*
+// c4 = c0 * (pg[0,3] + gg[0,3])
+wire pg_or_gg0, pg_or_gg1;
+OR or_pg_gg0(pg[0], gg[0], pg_or_gg0);
+AND and_c0_pggg(pg_or_gg0, cin, cout[0]);
+// c8 = c4 * (pg[4,7] + gg[4,7])
+OR or_pg_gg4(pg[1], gg[1], pg_or_gg1);
+AND and_pg_gg4(pg_or_gg1, cout[0], cout[1]);
+*/
+wire [5:0]w;
+
+AND and_c1(cin, pg[0], w[0]);
+OR or_c1(gg[0], w[0], cout[0]);
+
+AND and_c2(pg[1], gg[0], w[1]);
+AND_4bits and_c21(pg[1], pg[0], cin, 1'b1, w[2]);
+OR_4bits or_c2(gg[1], w[1], w[2], cout[1]);
+
+endmodule
+
+module AND_4bits(a, b, c, d, out);
+input a, b, c, d;
+output out;
+
+wire w0, w1;
+
+AND and0(a, b, w0);
+AND and1(c, d, w1);
+AND and2(w0, w1, out);
+
+endmodule
+
+module OR_4bits(a, b, c, d, out);
+input a, b, c, d;
+output out;
+
+wire w0, w1;
+
+OR and0(a, b, w0);
+OR and1(c, d, w1);
+OR and2(w0, w1, out);
+
+endmodule
+
+module AND(a, b, out);
+input a, b;
+output out;
+
+wire a_nand_b;
+
+nand nand_a_b(a_nand_b, a, b);
+NOT and_a_b(a_nand_b, out);
+
+endmodule
+
+module OR(a, b, f);
+input a,b;
+output f;
+
+wire a_neg, b_neg;
+
+NOT not_a(a, a_neg);
+NOT not_b(b, b_neg);
+nand nand_a_b(f, a_neg, b_neg);
+
+endmodule
+
+module NOT(in, out);
+input in;
+output out;
+
+nand not_in(out, in, in);
+
+endmodule
+
+module XOR(a, b, out);
+input a, b;
+output out;
+
+wire a_neg, b_neg, w0, w1;
+
+NOT not_a(a, a_neg);
+NOT not_b(b, b_neg);
+
+AND and_a_negb(a, b_neg, w0);
+AND and_nega_b(a_neg, b, w1);
+
+OR xor_a_b(w0, w1, out);
+
+endmodule
